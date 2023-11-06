d4b66b52b05d9058b2c6c0aa89a6bb183f634fd2
Hana Spoustová <bernreih@gmail.com>
2023-11-06 17:46:10 +0100

    zalozeni projektu react

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,14 @@
+import './style.css';
+
+export const HomePage = () => {
+  return (
+    <div className="container">
+      <header>
+        <h1>Projekt Gastro Monster</h1>
+      </header>
+
+      <main></main>
+      <footer></footer>
+    </div>
+  );
+};
