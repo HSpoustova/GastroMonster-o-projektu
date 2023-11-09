ec82826fa43490b99dc3cdb257fe12d7f4d3f4df
Hana Spoustová <bernreih@gmail.com>
2023-11-09 16:10:30 +0100

    pridani stranky Hana Spoustova

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -3,47 +3,46 @@ import monsterBaby from './img/babyMonsterHappy15-remove.png';
 import monsterKid from './img/kidMonsterHappy15-remove.png';
 import monsterAdult from './img/adultMonsteNeutral15-remove.png';
 
-
 export const Main = () => {
   return (
     <>
       <div className="main__container">
-          <div className="main__content">
-            <p>
-              Gastro Monster je edukační projekt zaměřený zejména na děti na
-              základní škole. Jeho hlavním cílem je vzdělávat v oblasti výživy a
-              výroby potravin.
-            </p>
+        <div className="main__content">
+          <p>
+            Gastro Monster je edukační projekt zaměřený zejména na děti na
+            základní škole. Jeho hlavním cílem je vzdělávat v oblasti výživy a
+            výroby potravin.
+          </p>
 
-            <p>
-              Mechanismem hry je postup skrze čtyři tematické oblasti otázek.
-              Konkrétně se týkají masa a masných výrobků, mléka a mléčných
-              výrobku, pekařských výrobků a oblast ovoce a zeleniny. Hráč má pro
-              průchod políčkovou oblastí přidělený omezený počet potravin, které
-              používají pro její průchod. Zároveň v případě správných odpovědu
-              krmí monstrum, které roste od mláděte až po dospělého.
-            </p>
+          <p>
+            Mechanismem hry je postup skrze čtyři tematické oblasti otázek.
+            Konkrétně se týkají masa a masných výrobků, mléka a mléčných
+            výrobku, pekařských výrobků a oblast ovoce a zeleniny. Hráč má pro
+            průchod políčkovou oblastí přidělený omezený počet potravin, které
+            používají pro její průchod. Zároveň v případě správných odpovědu
+            krmí monstrum, které roste od mláděte až po dospělého.
+          </p>
 
-            <p>
-              Hra různé prvky hry, jako je náhodné generování otázek, obměny
-              nových otázek po špatných odpovědích, pohyb malého avatara na
-              herním poli, komunikaci monstra během hry či poskytování nápověd.
-            </p>
-          </div>
+          <p>
+            Hra má různé prvky, jako je náhodné generování otázek, obměny nových
+            otázek po špatných odpovědích, pohyb malého avatara na herním poli,
+            komunikaci monstra během hry či poskytování nápověd.
+          </p>
         </div>
-        <div className="main__img">
-          <div className="main__content---baby">
-            <img src={monsterBaby} alt="Baby monstrum"></img>
-          </div>
-
-          <div className="main__content---kid">
-            <img src={monsterKid} alt="Dětské monstrum"></img>
-          </div>
-
-          <div className="main__content---adult">
-            <img src={monsterAdult} alt="Dospělé monstrum"></img>
-          </div>
+      </div>
+      <div className="main__img">
+        <div className="main__content---baby">
+          <img src={monsterBaby} alt="Baby monstrum"></img>
         </div>
+
+        <div className="main__content---kid">
+          <img src={monsterKid} alt="Dětské monstrum"></img>
+        </div>
+
+        <div className="main__content---adult">
+          <img src={monsterAdult} alt="Dospělé monstrum"></img>
+        </div>
+      </div>
     </>
   );
 };
--- src/pages/HanaSpoust/index.jsx
+++ src/pages/HanaSpoust/index.jsx
@@ -1,10 +1,23 @@
 import './style.css';
+import HankaFoto from './img/Hanka.jpg';
 
 export const HanaSpoust = () => {
   return (
     <>
-      <div>
-        <h1>Tady bude info</h1>
+      <div className="hanka_container">
+        <h1>Hanka Spoustová</h1>
+
+        <div className="hanka__img">
+          <img src={HankaFoto} alt="Hana Spoustová"></img>
+
+          <p>
+            Jsem mladá, perspektivní komunikativní osoba se smyslem pro humor,
+            programování a pro detail. Kouknout na to se můžete na
+            <strong>
+              <a href="https://github.com/HSpoustova">můj GitHub.</a>
+            </strong>
+          </p>
+        </div>
       </div>
     </>
   );
